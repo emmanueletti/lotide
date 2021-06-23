f002e32f7369c5c8ae2049e837cbfc7c66a36a04
Emmanuel Etti <emmanueletti@gmail.com>
2021-06-23 21:29:19 +0000

    added a try catch block to cleanly handle errors

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,22 @@
+// POTENTIAL BUG: calling every() on an empty array will always result in true
+// (e.g [].every() evalutes to true no matter the callback condition passed in)
+// assert function will result in false positive when the array being called on is empty but the array being compared to is not 
+// to prevent a false positive, call every() on both expected and actual arrays, and require a true positive to be a true result from both calls
+
 const assertArraysEqual = (actual, expected) => {
-  // POTENTIAL BUG: calling every() on an empty array will always result in true
-  // (e.g [].every() evalutes to true no matter the callback condition passed in)
-  // assert function will result in false positive when the array being called on is empty but the array being compared to is not 
-  // to prevent a false positive, call every() on both expected and actual arrays, and require a true positive to be a true result from both calls
+  try {
+    const everyActualEquals = actual.every((element, index) => element === expected[index]);
+	  const everyExpectedEquals = expected.every((element, index) => element === actual[index]);
 
-	const everyActualEquals = actual.every((element, index) => element === expected[index]);
-	const everyExpectedEquals = expected.every((element, index) => element === actual[index]);
-
-	if (everyActualEquals && everyExpectedEquals) {
-		console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-	} else {
-		console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-	}
+      if (everyActualEquals && everyExpectedEquals) {
+      console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+      console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+  } catch (error) {
+    console.log('😬 Something broke')
+    console.log(`\t ${error.name}: ${error.message}`)
+  }
 };
 
 console.log('\n --correctness checks--')
--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,19 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+
+  try {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+      console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+  } catch (error) {
+    console.log('😬 Something broke');
+    console.log(`\t ${error.name}: ${error.message}`);
   }
+
+ 
 };
+
 
 //TEST
 assertEqual('Lighthouse Labs', 'Bootcamp');
