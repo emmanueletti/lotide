1f473e64262f5efefc35f55e1f35da70c620251d
Emmanuel Etti <emmanueletti@gmail.com>
2021-06-28 21:23:18 +0000

    added mocha and chai testing. Updated head.test.js to use mocha and chai for its testing

--- test/head.test.js
+++ test/head.test.js
@@ -1,7 +1,20 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const { assert } = require('chai');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([], undefined));
-assertEqual(head([2]), 2);
+describe('Head Function', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+    // expect(head([1, 2, 3])).to.equal(1);
+  });
+
+  it(`returnes '5' for ['5']`, () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it(`should return 'undefined' for []`, () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  //
+});
